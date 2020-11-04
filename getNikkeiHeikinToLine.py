7963115ca696f04684ec257210ae39b799d7e54d
yusaku.eguchi <yusaku.eguchi@sharing-innovations.co.jp>
2020-11-04 14:39:02 +0900

    コールバック処理のみ記載

--- getNikkeiHeikinToLine.py
+++ getNikkeiHeikinToLine.py
@@ -16,10 +16,19 @@ from linebot.models import(
     MessageEvent, TextMessage, TextSendMessage
 )
 
+#ログ出力のため
+import logging
+import sys
+
 app = Flask(__name__)
 
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+# ログを標準出力へ。heroku logs --tail で確認するためです。
+# app.logger.info で出力するため、レベルは INFO にする。
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.INFO)
+
+YOUR_CHANNEL_ACCESS_TOKEN = '7daN8C8fvz/Mb614TwGrXSIPOA/rNYopIQHWtfpKWn6StPCG+/gVS0JhO38sMIx04OGqjS21IbKwXazaETM4BIQPjjNfzGNour2cUO7etCbYdHwGA5pbeLFmDBhgAHaujyFcJeUkKY0LGz7nMTAf3gdB04t89/1O/w1cDnyilFU='
+YOUR_CHANNEL_SECRET = '13f10b72997410fab6886825485b561c'
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
@@ -47,45 +56,14 @@ def callback():
 
    return 'OK'
 
+@handler.add(MessageEvent, message=TextMessage)
+def reply_message(event):
+    # reply のテスト。
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='こちらこーるばっく処理からお送りします:'+event.message.text))
 
 
-
-# 現在時刻を年、月、日、分、秒で取得します
-time_ = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-
-# アクセスするURL
-url = "http://www.nikkei.com/markets/kabu/"
-
-# URLにアクセスする htmlが帰ってくる → <html><head><title>経済、株価、ビジネス、政治のニュース:日経電子版</title></head><body....
-html = urllib.request.urlopen(url)
-
-# htmlをBeautifulSoupで扱う
-soup = BeautifulSoup(html, "html.parser")
-
-# span要素全てを摘出する→全てのspan要素が配列に入ってかえされます→[<span class="m-wficon triDown"></span>, <span class="l-h...
-span = soup.find_all("span")
-
-# print時のエラーとならないように最初に宣言しておきます。
-nikkei_heikin = ""
-# for分で全てのspan要素の中からClass="mkc-stock_prices"となっている物を探します
-for tag in span:
-    # classの設定がされていない要素は、tag.get("class").pop(0)を行うことのできないでエラーとなるため、tryでエラーを回避する
-    try:
-        # tagの中からclass="n"のnの文字列を摘出します。複数classが設定されている場合があるので
-        # get関数では配列で帰ってくる。そのため配列の関数pop(0)により、配列の一番最初を摘出する
-        # <span class="hoge" class="foo">  →   ["hoge","foo"]  →   hoge
-        string_ = tag.get("class").pop(0)
-
-        # 摘出したclassの文字列にmkc-stock_pricesと設定されているかを調べます
-        if string_ in "mkc-stock_prices":
-            # mkc-stock_pricesが設定されているのでtagで囲まれた文字列を.stringであぶり出します
-            nikkei_heikin = tag.string
-            # 摘出が完了したのでfor分を抜けます
-            break
-    except:
-        # パス→何も処理を行わない
-        pass
-    
 if __name__ == "__main__":
-   port = int(os.getenv("PORT"))
-   app.run(host="0.0.0.0", port=port)
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
