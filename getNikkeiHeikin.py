4d822068db9dacd620bd1619caa2802c87342e94
yusaku.eguchi <yusaku.eguchi@sharing-innovations.co.jp>
2020-11-08 01:18:03 +0900

    修正

--- getNikkeiHeikin.py
+++ getNikkeiHeikin.py
@@ -3,21 +3,17 @@ import urllib.request, urllib.error
 from bs4 import BeautifulSoup
 
 # アクセスするURL
+url = "http://www.nikkei.com/markets/kabu/"
+# URLにアクセスする htmlが帰ってくる → <html><head><title>経済、株価、ビジネス、政治のニュース:日経電子版</title></head><body....
+html = urllib.request.urlopen(url)
+# htmlをBeautifulSoupで扱う
+soup = BeautifulSoup(html, "html.parser")
+# span要素全てを摘出する→全てのspan要素が配列に入ってかえされます→[<span class="m-wficon triDown"></span>, <span class="l-h...
+span = soup.find_all("span")
+# print時のエラーとならないように最初に宣言しておきます。
+# for分で全てのspan要素の中からClass="mkc-stock_prices"となっている物を探します
 def get_nikkei_heikin():
-    url = "http://www.nikkei.com/markets/kabu/"
-
-    # URLにアクセスする htmlが帰ってくる → <html><head><title>経済、株価、ビジネス、政治のニュース:日経電子版</title></head><body....
-    html = urllib.request.urlopen(url)
-
-    # htmlをBeautifulSoupで扱う
-    soup = BeautifulSoup(html, "html.parser")
-
-    # span要素全てを摘出する→全てのspan要素が配列に入ってかえされます→[<span class="m-wficon triDown"></span>, <span class="l-h...
-    span = soup.find_all("span")
-
-    # print時のエラーとならないように最初に宣言しておきます。
     nikkei_heikin = ""
-    # for分で全てのspan要素の中からClass="mkc-stock_prices"となっている物を探します
     for tag in span:
         # classの設定がされていない要素は、tag.get("class").pop(0)を行うことのできないでエラーとなるため、tryでエラーを回避する
         try:
@@ -25,7 +21,6 @@ def get_nikkei_heikin():
             # get関数では配列で帰ってくる。そのため配列の関数pop(0)により、配列の一番最初を摘出する
             # <span class="hoge" class="foo">  →   ["hoge","foo"]  →   hoge
             string_ = tag.get("class").pop(0)
-
             # 摘出したclassの文字列にmkc-stock_pricesと設定されているかを調べます
             if string_ in "mkc-stock_prices":
                 # mkc-stock_pricesが設定されているのでtagで囲まれた文字列を.stringであぶり出します
@@ -36,4 +31,4 @@ def get_nikkei_heikin():
             # パス→何も処理を行わない
             pass
     print(nikkei_heikin)
-    return nikkei_heikin    
+    return(nikkei_heikin)  
